35755fa9f0d1c111b2f3a02cbc153084ac782e50
Tekiter <tekiter8@gmail.com>
2020-11-24 23:34:00 +0900

    Tokenizer 구현 완료

--- src/tokenize.spec.ts
+++ src/tokenize.spec.ts
@@ -1,20 +1,60 @@
-import { Tokenizer } from './tokenize';
+import { Tokenizer } from './tokenizer';
 
-test('readToken 이 Token 하나를 잘 가져와야함', () => {
-    const input: string = '   a \t b \n c';
+test('readToken이 Token 하나를 잘 가져와야함', () => {
+    let input = 'a \t b \n c';
 
     let tokenizer = new Tokenizer(input);
     expect(tokenizer.readToken()).toBe('a');
     expect(tokenizer.readToken()).toBe('b');
     expect(tokenizer.readToken()).toBe('c');
+
+    input = '\n\ta \nb\t\t c\n\n';
+    tokenizer = new Tokenizer(input);
+    expect(tokenizer.readToken()).toBe('a');
+    expect(tokenizer.readToken()).toBe('b');
+    expect(tokenizer.readToken()).toBe('c');
 });
 
-test('readLine 이 한 줄을 잘 가져와야함', () => {
+test('readLine이 한 줄을 잘 가져와야함', () => {
     let input: string;
 
-    input = 'hello world\nworld hello';
+    input = 'hello world\n\nworld hello';
 
     let tokenizer = new Tokenizer(input);
     expect(tokenizer.readLine()).toBe('hello world');
+    expect(tokenizer.readLine()).toBe('');
     expect(tokenizer.readLine()).toBe('world hello');
+});
+
+test('readToken과 readLine을 섞어도 잘 작동해야함', () => {
+    let input: string;
+
+    input = 'a\t b c \nd   e f \t\n g';
+
+    let tokenizer = new Tokenizer(input);
+    expect(tokenizer.readToken()).toBe('a');
+    expect(tokenizer.readToken()).toBe('b');
+    expect(tokenizer.readToken()).toBe('c');
+    expect(tokenizer.readToken()).toBe('d');
+    expect(tokenizer.readLine()).toBe('   e f \t');
+    expect(tokenizer.readToken()).toBe('g');
+});
+
+test('readToken과 readLine이 읽을게 없으면 에러를 띄워야함', () => {
+    let input: string;
+
+    input = 'a b ';
+
+    let tokenizer = new Tokenizer(input);
+
+    expect(tokenizer.readToken()).toBe('a');
+    expect(tokenizer.readToken()).toBe('b');
+    expect(() => tokenizer.readToken()).toThrow(Error);
+
+    input = 'a\n';
+
+    tokenizer = new Tokenizer(input);
+
+    expect(tokenizer.readLine()).toBe('a');
+    expect(() => tokenizer.readToken()).toThrow(Error);
 });
---
+++ src/tokenizer.ts
@@ -0,0 +1,82 @@
+interface StringReader {
+    readable: boolean;
+    read(): string;
+    peek(): string;
+}
+class TokenizeReader implements StringReader {
+    target: string;
+    cursor: number;
+
+    constructor(target: string) {
+        this.target = target;
+        this.cursor = 0;
+    }
+
+    get readable(): boolean {
+        return this.cursor < this.target.length;
+    }
+
+    read(): string {
+        return this.target[this.cursor++];
+    }
+
+    peek(): string {
+        return this.target[this.cursor];
+    }
+}
+
+const TokenDelimiter: readonly string[] = ['\n', '\t', ' '] as const;
+const LineDelimiter: readonly string[] = ['\n'] as const;
+
+export class Tokenizer {
+    private reader: StringReader;
+
+    constructor(rawString: string) {
+        this.reader = new TokenizeReader(rawString);
+    }
+
+    readToken(delimiter: readonly string[] = TokenDelimiter): string {
+        const buffer: string[] = [];
+
+        while (this.reader.readable) {
+            const cur = this.reader.peek();
+
+            if (delimiter.includes(cur)) {
+                if (buffer.length === 0) {
+                    this.reader.read();
+                    continue;
+                } else {
+                    break;
+                }
+            } else {
+                buffer.push(cur);
+            }
+
+            this.reader.read();
+        }
+        if (!this.reader.readable && buffer.length === 0) {
+            throw new Error('더 읽을 Token이 없습니다.');
+        }
+
+        return buffer.join('');
+    }
+
+    readLine(): string {
+        const buffer: string[] = [];
+
+        while (this.reader.readable) {
+            const cur = this.reader.peek();
+
+            if (LineDelimiter.includes(cur)) {
+                this.reader.read();
+                break;
+            } else {
+                buffer.push(cur);
+            }
+
+            this.reader.read();
+        }
+
+        return buffer.join('');
+    }
+}
